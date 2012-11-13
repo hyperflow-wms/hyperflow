34031c4eac880eb4bca46d86a9f3904c95c92420
Bartek <balis@agh.edu.pl>
2012-11-12 17:02:58 -0700

    Unknown update

--- deltawf/index.js
+++ deltawf/index.js
@@ -1,56 +1,56 @@
 /* 2012 (bb) : hypermedia workflow, delta resource implementation */
 
 exports.init = function() {
-				var deltaAll = []; // table of all delta resources (private)
+	var deltaAll = []; // table of all delta resources (private)
 
-				function public_create(id) {
-								deltaAll[id] = {
-												"events" : []
-								};
+	function public_create(id) {
+		deltaAll[id] = {
+			"events" : []
+		};
+	}
+
+	function public_addEvent(id, key, value) {
+		if (id in deltaAll) {
+			var now=new Date().getTime(); 
+			deltaAll[id].events.push({'time': now, 'key': key, 'value': value});
+		} else {
+			throw("ENODELTA");
+		}
+	}
+
+	function public_getDelta(id, from) {
+		var res = [];
+		if (id in deltaAll) {
+			foreach(deltaAll[id].events, function(ev) {
+				if (parseInt(ev.time) > parseInt(from)) {
+					res.push({'k': ev.key, 'v': ev.value});
 				}
+			});
+			return res;
+		} else {
+			throw("ENODELTA");
+		}
+	}
 
-				function public_addEvent(id, key, value) {
-								if (id in deltaAll) {
-												var now=new Date().getTime(); 
-												deltaAll[id].events.push({'time': now, 'key': key, 'value': value});
-								} else {
-												// no delta resource with given id. TODO: throw error?
-								}
-				}
+	return {
+		create: public_create,
+		addEvent: public_addEvent,
+		getDelta: public_getDelta
+	};
 
-				function public_getDelta(id, from) {
-								var res = [];
-								if (id in deltaAll) {
-												foreach(deltaAll[id].events, function(ev) {
-																if (parseInt(ev.time) > parseInt(from)) {
-																				res.push({'k': ev.key, 'v': ev.value});
-																}
-												});
-												return res;
-								} else {
-												// no delta resource with given id. TODO: throw error?
-								}
-				}
+	// private functions
+	function foreach(what, cb) {
+		function isArray(what) {
+			return Object.prototype.toString.call(what) === '[object Array]';
+		}
 
-				return {
-								create: public_create,
-								addEvent: public_addEvent,
-								getDelta: public_getDelta
-				};
-
-				// private functions
-				function foreach(what, cb) {
-								function isArray(what) {
-												return Object.prototype.toString.call(what) === '[object Array]';
-								}
-
-								if (isArray(what)) {
-												for (var i = 0, arr = what; i < what.length; i++) {
-																cb(arr[i]);
-												}
-								}
-								else {
-												cb(what);
-								}
-				}
+		if (isArray(what)) {
+			for (var i = 0, arr = what; i < what.length; i++) {
+				cb(arr[i]);
+			}
+		}
+		else {
+			cb(what);
+		}
+	}
 };
