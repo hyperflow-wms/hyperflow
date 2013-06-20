d19ba279fb18f07199bd1b412a5bf984da295559
wojtasskorcz <wojtasskorcz@gmail.com>
2013-06-20 22:25:24 +0200

    Added descriptions to Fun, Signal, SimpleSignal, Task

--- wfdsl/src/main/scala/app/generator/Fun.scala
+++ wfdsl/src/main/scala/app/generator/Fun.scala
@@ -1,5 +1,9 @@
 package app.generator
 
+/*
+ * Represents a function. 
+ * globalIndex defaults to the order of declaration (starting from 0)
+ */
 case class Fun(val module: String, val name: String, val globalIndex: Int) {
 
 }
