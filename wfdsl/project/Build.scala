6f0c0d39f3aa396c61d1fcd0edf149a43c4739ed
falconepl <falconepl@gmail.com>
2013-06-21 02:20:57 +0200

    Added JAR generation task to the SBT.

---
+++ wfdsl/project/Build.scala
@@ -0,0 +1,19 @@
+import com.github.retronym.SbtOneJar
+import sbt._
+import Keys._
+
+object HelloBuild extends Build {
+  val hwsettings = Defaults.defaultSettings ++ Seq(
+    name := "kompilatory",
+    version := "1.0.0",
+    scalaVersion := "2.10.1",
+    scalacOptions ++= Seq("-deprecation", "-feature"),
+    libraryDependencies += "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test",
+    libraryDependencies += "junit" % "junit" % "4.10" % "test",
+    exportJars := true)
+
+  lazy val project = Project(
+    id = "kompilatory",
+    base = file("."),
+    settings = hwsettings ++ SbtOneJar.oneJarSettings)
+}
