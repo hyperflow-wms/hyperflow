7746fcc4a297ce98e60faf4f82d6b28e4db06133
Maciej Pawlik <m.pawlik@cyfronet.pl>
2013-09-28 22:17:34 +0200

    Added sending message, and subscribing to queue

--- executor_amqp/index.js
+++ executor_amqp/index.js
@@ -24,9 +24,38 @@ exports.init = function() {
     function public_execute(task, server, cb) {
 	
 //    	console.log(task)
+    
+	var connection = amqp.createConnection( { host: '149.156.10.132', port: 19164 } );
+	
+	connection.on('ready', function() {
+//		console.log('connection ready');
+		var q = connection.queue ('', { exclusive: true }, function(queue) {
+			console.log('exclusive queue declared ' + queue.name);
+			var corrId = uuid.v4();
+			
+			queue.subscribe({ exclusive: true }, function(message, headers, deliveryInfo) {
+				//console.log(JSON.parse(message.data.toString()));
+				if(deliveryInfo.correlationId == corrId) {
+					console.log('corr id match!, message:');
+					console.log(message);
+					//TODO: Add proper interpretation of job outcome, throw some output on screen
+					cb(null, 0);
+				} else {
+					console.log("unexpected message");
+				}
+			});
+			
+			//publish job
+			var exchange = connection.exchange('', {}, function(exchange) {
+				exchange.publish('hyperflow.jobs', '{ "executable" : "/bin/ls", "args": "", "inputs": [], "outputs": [] }',
+						{replyTo: queue.name, contentType: 'application/json', correlationId: corrId});
+			});
+			
+		});
+	});
     	
 	setTimeout(function() {
-		cb(null, 'some output');
+//		check if this can be removed?
 	}, 1000);
 	
     }
