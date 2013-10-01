cd34c5d6154df6b42b51e80d24d9b6cea6e375f6
Maciej Pawlik <m.pawlik@cyfronet.pl>
2013-10-01 11:39:52 +0200

    Add passing command in message.

--- functions/amqpCommand.js
+++ functions/amqpCommand.js
@@ -1,5 +1,6 @@
 var amqp = require('amqp');
 var uuid = require('uuid');
+var util = require('util');
 
 function amqpCommand(ins, outs, executor, config, cb) {
 	
@@ -9,26 +10,37 @@ function amqpCommand(ins, outs, executor, config, cb) {
 	var connection = amqp.createConnection( { host: 'localhost', port: 5672 } );
 
 	connection.on('ready', function() {
+		console.trace();
 		console.log("connection ready");
+		var ctag;
 		var q = connection.queue ('', { exclusive: true }, function(queue) {
 			console.log("queue ready");
 			var corrId = uuid.v4();
 
-			queue.subscribe({ exclusive: true }, function(message, headers, deliveryInfo) {
+			queue.subscribe({ exclusive: true, ack: true }, function(message, headers, deliveryInfo) {
 				console.log("message recieved");
 				if(deliveryInfo.correlationId == corrId) {
-					console.log('corr id match!');
 					//TODO: Add proper interpretation of job outcome, throw some output on screen
+					console.log("output:" + message.stdout + ", stderr:" + message.stderr + ", ret code:" + message.return_code);
+//					q.unsubscribe(ctag);
+					try {
+						console.log(util.inspect(connection));
+						connection.end();
+					} catch (e) {
+						console.log(e);
+					}
 					cb(null, outs);
 				} else {
 					console.log("unexpected message");
 				}
+			}).addCallback(function (ok) {
+				ctag = ok.consumerTag;
 			});
 		
 			//publish job
 			var exchange = connection.exchange('', {}, function(exchange) {
 				console.log("job published");
-				exchange.publish('hyperflow.jobs', '{ "executable" : "/bin/ls", "args": "", "inputs": [], "outputs": [] }',
+				exchange.publish('hyperflow.jobs', { "executable" : executable, "args": args, "inputs": [], "outputs": [] },
 						{replyTo: queue.name, contentType: 'application/json', correlationId: corrId});
 			});				
 		});
