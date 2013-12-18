095a2142df5959063ec29ed5111634e5505ea795
Kamil Figiela <kamil.figiela@gmail.com>
2013-12-18 23:45:29 +0100

    AMQP connection singleton done right way!

--- functions/amqpCommand.js
+++ functions/amqpCommand.js
@@ -5,80 +5,91 @@ var AMQP_URL = process.env.AMQP_URL ? process.env.AMQP_URL : "amqp://localhost:5
 var S3_BUCKET = process.env.S3_BUCKET;
 var S3_PATH = process.env.S3_PATH;
 var connection = null;
+var connectionReady = false;
 
 function amqpCommand(ins, outs, executor, config, cb) {
-  var executable = config.executor.executable;
-  var args = config.executor.args;
-  var deliberatelyExit = false;
+  getConnection(function(connection) {
+    var executable = config.executor.executable;
+    var args = config.executor.args;
+    var deliberatelyExit = false;
 
-  var consumerTag;
-  var q = connection.queue('', {
-    exclusive: true,
-    closeChannelOnUnsubscribe: true
-  }, function(queue) {
-    var corrId = uuid.v4();
-
-    queue.subscribe({
+    var consumerTag;
+    var q = connection.queue('', {
       exclusive: true,
-      ack: true
-    }, function(message, headers, deliveryInfo) {
-      console.log("[AMQP Command][" + corrId + "] message recieved");
-      if (deliveryInfo.correlationId == corrId) {
-        console.log("[AMQP Command][" + corrId + "] result: output:", message.stdout, "stderr:", message.stderr, "ret code:", message.exit_code);
-        console.log(message);
-        queue.unsubscribe(consumerTag);
+      closeChannelOnUnsubscribe: true
+    }, function(queue) {
+      var corrId = uuid.v4();
 
-        if (message.exit_code != "0") {
-          cb(null, outs);
+      queue.subscribe({
+        exclusive: true,
+        ack: true
+      }, function(message, headers, deliveryInfo) {
+        if (deliveryInfo.correlationId == corrId) {
+          console.log("[AMQP][" + corrId + "] Job finished: " + JSON.stringify(message, null, "  "));
+          queue.unsubscribe(consumerTag);
+
+          if (message.exit_code != "0") {
+            cb(null, outs);
+          } else {
+            cb("[AMQP][" + corrId + "] Error during job execution!", outs);
+          }
         } else {
-          cb("[AMQP Command][" + corrId + "] Error during job execution!", outs);
+          console.log("[AMQP][" + corrId + "] Invalid message id");
+          cb("[AMQP][" + corrId + "] Invalid message id")
         }
-      } else {
-        console.log("[AMQP Command][" + corrId + "] unexpected message, got: ", deliveryInfo.correlationId, "expected:", corrId);
-        cb("[AMQP Command][" + corrId + "] Unexpected message")
-      }
-    }).addCallback(function(ok) {
-      consumerTag = ok.consumerTag;
-    });
+      }).addCallback(function(ok) {
+        consumerTag = ok.consumerTag;
+      });
 
-    //publish job
-    var exchange = connection.exchange('', {}, function(exchange) {
-      console.log("[AMQP Command] job published");
-      var message = {
-        "executable": executable,
-        "args": args,
-        "inputs": ins,
-        "outputs": outs,
-        "options": {
-          "bucket": S3_BUCKET,
-          "prefix": S3_PATH,
-        }
-      };        
-      console.log(message);
-      exchange.publish('hyperflow.jobs', message, {
-        replyTo: queue.name,
-        contentType: 'application/json',
-        correlationId: corrId
+      //publish job
+      var exchange = connection.exchange('', {}, function(exchange) {
+        console.log("[AMQP][" + corrId + "] Job published");
+        var message = {
+          "executable": executable,
+          "args": args,
+          "inputs": ins,
+          "outputs": outs,
+          "options": {
+            "bucket": S3_BUCKET,
+            "prefix": S3_PATH,
+          }
+        };
+        exchange.publish('hyperflow.jobs', message, {
+          replyTo: queue.name,
+          contentType: 'application/json',
+          correlationId: corrId
+        });
       });
     });
   });
 }
 
-function connect(cb) {
-  connection = amqp.createConnection({
-    url: AMQP_URL,
-    heartbeat: 30
-  });
+function getConnection(cb) {
+  if (connection) {
+    if (connectionReady) {
+      cb(connection);
+    } else {
+      connection.once('ready', function(){ cb(connection); });
+    }
+  } else {
+    connection = amqp.createConnection({
+      url: AMQP_URL,
+      heartbeat: 30
+    });
 
-  connection.on('ready', function() {
-    console.log('[AMQP Command] Connection ready')
-    cb();
-  });
-  connection.on('error', function(err) {
-    console.log("[AMQP Command] Error: ", err);
-  });
-
+    connection.once('ready', function() {
+      connectionReady = true;
+      console.log('[AMQP] Connection ready')
+      cb(connection);
+    });
+    
+    connection.once('error', function(err) {
+      connection = null;
+      connectionReady = false;
+      console.log("[AMQP] Error: ", err);
+      throw err;
+    }).setMaxListeners(0);
+  }
 };
 
-exports.connect = connect;
 exports.amqpCommand = amqpCommand;
