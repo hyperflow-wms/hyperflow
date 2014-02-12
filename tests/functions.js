b906aae6bf72a52400b3ff35f6a93a045188f2fc
Maciej Pawlik <m.pawlik@cyfronet.pl>
2014-02-12 14:58:38 +0100

    Added test_function_notifyevents_with_eventserver.

--- tests/functions.js
+++ tests/functions.js
@@ -1,18 +1,33 @@
 var functions = require('../functions')
 
-exports.test_function_notifyevents = function(test) {
+exports.setUp = function(callback) {
+    this.config = {
+        executor: {
+            executable: "ls",
+            args: "-la"
+        }
+    };
+    callback();
+}
+
+exports.test_function_notifyevents_with_eventserver = function(test) {
+    var notified = false;
     var ins = [],
         outs = [];
-    var config = {
-      executor: {
-          executable: "ls",
-          args: "-la"
-      }
-    };
-    var cb = function(exceptions, outs) {
 
+    this.config.eventserver = {
+        emit: function(type, arg1, arg2) {
+            notified = true;
+        }
     };
-    functions.command_print(ins, outs, config, cb);
+
+    var cb = function(exceptions, outs) {
+        test.equal(exceptions, null);
+    };
+
+
+    functions.command_notifyevents(ins, outs, this.config, cb);
+    test.ok(notified);
 
     test.done()
 }
