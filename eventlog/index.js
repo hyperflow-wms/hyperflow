e168ea2f12c7050eb67d85d01cfed9be1aab0319
Maciej Pawlik <m.pawlik@cyfronet.pl>
2014-02-24 22:26:42 +0100

    Change eventlog to return new eventserver instance on request.

--- eventlog/index.js
+++ eventlog/index.js
@@ -1,34 +1,42 @@
-var EventEmitter2 = require('eventemitter2').EventEmitter2,
-    server = new EventEmitter2({
-      wildcard: true, // should the event emitter use wildcards.
-      delimiter: '.', // the delimiter used to segment namespaces, defaults to `.`.
-      newListener: false, // if you want to emit the newListener event set to true.
-      maxListeners: 20 // the max number of listeners that can be assigned to an event, defaults to 10.
+var EventEmitter2 = require('eventemitter2').EventEmitter2
+
+var EventServer = function () {
+    var server = new EventEmitter2({
+        wildcard: true, // should the event emitter use wildcards.
+        delimiter: '.', // the delimiter used to segment namespaces, defaults to `.`.
+        newListener: false, // if you want to emit the newListener event set to true.
+        maxListeners: 20 // the max number of listeners that can be assigned to an event, defaults to 10.
     });
+};
 
-function emit(ev, data) {
+EventServer.prototype.emit = function (ev, data) {
     data.time = new Date().toISOString(); // automatically add a timestamp to event data
-    server.emit(ev, data);
-}
+    this.emit(ev, data);
+};
 
-function on(ev, listener) {
-    server.on(ev, listener);
-}   
+EventServer.prototype.on = function (ev, listener) {
+    this.on(ev, listener);
+};
+
+function createEventServer() {
+    var eventServer = new EventServer();
 
 // this object is exported as the API of the event logger (hides implementation)
-var eventlog = {
-    emit: emit,
-    on: on
+    var eventlog = {
+        emit: eventServer.emit,
+        on: eventServer.on
+    }
+// Here's how to subscribe to events:
+    eventlog.on('trace.*', function (data) {
+        console.log(arguments);
+        // "this.event" contains the full event name
+        console.log("EVENT:", this.event, JSON.stringify(data, null, 2));
+    });
+    return eventlog;
 }
 
-// Here's how to subscribe to events:
-server.on('trace.*', function(data) {
-    console.log(arguments);
-    // "this.event" contains the full event name
-    console.log("EVENT:", this.event, JSON.stringify(data, null, 2));
-});
 
-exports.eventlog = eventlog;
+exports.createEventlog = createEventServer();
 
 // Example use of the event logger from another module:
 // eventlog = require('../eventlog').eventlog,
