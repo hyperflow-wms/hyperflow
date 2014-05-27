11811689a1e0a7efb263e7ef447d3bd0dfc69bc8
Maciej Pawlik <m.pawlik@cyfronet.pl>
2014-05-27 21:34:06 +0200

    Basic prov data is stored in neo4j instance.

--- provenance_store/index.js
+++ provenance_store/index.js
@@ -1,3 +1,6 @@
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase('http://localhost:7474');
+
 function handle_data(data, args) {
     var op = args[0],
         appId = args[1],
@@ -7,6 +10,29 @@ function handle_data(data, args) {
         sigIdx = args[5];
 
     console.log("o:", op, appId, "p:", procId, "f:", firingId, "sid:", sigId, "sidx:", sigIdx);
+
+    params = {
+        'op': op,
+        'appId': appId,
+        'procId': procId,
+        'firingId': firingId
+    };
+
+    if (sigId) {
+        params['sigId'] = sigId;
+    }
+    if (sigIdx) {
+        params['sigIdx'] = sigIdx;
+    }
+
+    var node = db.createNode(params);
+    node.save(function (err, node) {
+        if (err) {
+            console.log("error saving to db!", err);
+        } else {
+            console.log('successful save:', node.id);
+        }
+    });
 }
 
 exports.handle_data = handle_data;
