f8ef94a1f9bd0cf1a154e5fd03a53984296ace41
Maciej Pawlik <m.pawlik@cyfronet.pl>
2014-05-27 21:08:16 +0200

    Added prov store placeholder.

--- provenance_store/index.js
+++ provenance_store/index.js
@@ -1,3 +1,12 @@
-/**
- * Created by yaq on 27.05.14.
- */
+function handle_data(data, args) {
+    var op = args[0],
+        appId = args[1],
+        procId = args[2],
+        firingId = args[3],
+        sigId = args[4],
+        sigIdx = args[5];
+
+    console.log("o:", op, appId, "p:", procId, "f:", firingId, "sid:", sigId, "sidx:", sigIdx);
+}
+
+exports.handle_data = handle_data;
