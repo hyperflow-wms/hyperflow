01521332deca84eb7f485b9ad935609b44f5adb5
Maciej Pawlik <m.pawlik@cyfronet.pl>
2014-05-27 23:41:43 +0200

    First try of relations.

--- provenance_store/index.js
+++ provenance_store/index.js
@@ -4,6 +4,8 @@ var async = require('async');
 var db = new neo4j.GraphDatabase('http://localhost:7474');
 var queue = async.queue(store_provenance_info, 1); //max concurrency
 
+db.query("MATCH (n) DELETE n", function() {});
+
 function store_provenance_info(args, callback) {
     var op = args[0],
         appId = args[1],
@@ -29,19 +31,51 @@ function store_provenance_info(args, callback) {
     }
 
     var node = db.createNode(params);
+
     node.save(function (err, node) {
         if (err) {
             console.log("error saving to db!", err);
         } else {
             console.log('successful save:', node.id);
+            //check for related nodes
+            if (op == "read") {
+                var reverse_op = "write";
+                //TODO: needs to include firingId as a parameter
+                var query = [
+                    "MATCH (n {",
+                        "op:\"" + reverse_op + "\",",
+                        "sigId:" + sigId + ",",
+                        "sigIdx:" + sigIdx,
+                    "}) return n"
+                ].join(" ");
+                db.query(query, function (err, results) {
+                    if (err) {
+                        console.log("Query error!", err);
+                    } else {
+                        //should return at most one!
+                        for(var i=0; i<results.length; i++) {
+//                            console.log(">>>>>>>>>>>>>", JSON.stringify(results[i]["n"].id));
+                            var related_node = results[i]["n"];
+                            //if there are any, create relation
+                            related_node.createRelationshipTo(node, 'produces signal for', {}, function(err, rel) {
+                                if(err) {
+                                    console.log("Error creating relation!", err);
+                                }
+                            });
+                        }
+                    }
+                });
+            }
         }
         callback();
     });
+
+
 }
 
 function handle_data(data, args) {
     queue.push([ args ], function (err) {
-        if(err) {
+        if (err) {
             console.log("Error saving:", data, "err:", err);
         }
     });
