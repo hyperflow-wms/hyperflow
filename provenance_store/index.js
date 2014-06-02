b3576d29193f0e8e29fc7d4de3f2ace619704b4a
Maciej Pawlik <m.pawlik@cyfronet.pl>
2014-06-02 14:47:37 +0200

    Added firingId as a selector for connecting read and write to signal events.

--- provenance_store/index.js
+++ provenance_store/index.js
@@ -4,7 +4,8 @@ var async = require('async');
 var db = new neo4j.GraphDatabase('http://localhost:7474');
 var queue = async.queue(store_provenance_info, 1); //max concurrency
 
-db.query("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r", function() {});
+db.query("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r", function () {
+});
 
 function store_provenance_info(args, callback) {
     var op = args[0],
@@ -37,12 +38,14 @@ function store_provenance_info(args, callback) {
             console.log("error saving to db!", err);
         } else {
             console.log('successful save:', node.id);
+            //create relationships
             if (op == "read") {
+                //connect writing and reading a signal with relationship
                 var reverse_op = "write";
-                //TODO: needs to include firingId as a parameter
                 var query = [
                     "MATCH (n {",
                         "op:\"" + reverse_op + "\",",
+                        "firingId:" + firingId + ",",
                         "sigId:" + sigId + ",",
                         "sigIdx:" + sigIdx,
                     "}) return n"
@@ -52,18 +55,24 @@ function store_provenance_info(args, callback) {
                         console.log("Query error!", err);
                     } else {
                         //should return at most one!
-                        for(var i=0; i<results.length; i++) {
+                        for (var i = 0; i < results.length; i++) {
 //                            console.log(">>>>>>>>>>>>>", JSON.stringify(results[i]["n"].id));
                             var related_node = results[i]["n"];
                             //if there are any, create relation
-                            related_node.createRelationshipTo(node, 'produces signal for', {}, function(err, rel) {
-                                if(err) {
+                            related_node.createRelationshipTo(node, 'produces signal for', {}, function (err, rel) {
+                                if (err) {
                                     console.log("Error creating relation!", err);
+                                } else {
+                                    console.log("Created relationship for: ", node["data"]["procId"]);
                                 }
                             });
                         }
                     }
                 });
+            }
+            if (op == "write") {
+                //connect signals dependant according to being "in state"
+                
             }
         }
         callback();
