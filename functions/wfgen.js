04ba173de9c80a0d7cd6d1af6d9503a9c6631db4
Maciej Pawlik <m.pawlik@cyfronet.pl>
2014-09-30 16:23:27 +0200

    Basic threat calculation added.

--- functions/wfgen.js
+++ functions/wfgen.js
@@ -1,16 +1,37 @@
+var ThreatLevel = {
+    NONE: "none",
+    HEIGHTENED: "heightened",
+    SEVERE: "severe"
+};
+
 function computeScenarioRanks(ins, outs, config, cb) {
     var realDataURI = ins[0].data[0];
 
     console.log("Computing ranks...");
     console.log(ins[0].data);
 
-    outs.Ranks.data = [ [ ins[0].name, "rank1", "rank2", "rank3" ] ];
+    outs.Ranks.data = [
+        [ ins[0].name, "rank1", "rank2", "rank3" ]
+    ];
     cb(null, outs);
 }
 
 function calculateThreatLevel(ranksData) {
     console.log("I got this:");
-    console.log(ranksData);
+    ranks = [];
+    ranksData.forEach(function (entry) {
+        ranks.push(JSON.parse(entry));
+    });
+    console.log(ranks);
+
+    threatLevel = NONE;
+    ranks.forEach(function (entry) {
+        //let's assume that scenarios below 55 represent threatening situations
+        if (parseInt(entry.scenario_id) < 55) {
+            threatLevel = HEIGHTENED;
+        }
+    });
+    return threatLevel;
 }
 
 function computeThreatLevel(ins, outs, config, cb) {
@@ -18,7 +39,10 @@ function computeThreatLevel(ins, outs, config, cb) {
     console.log("   ranks:", ins.Ranks.data);
     console.log("   jobscount:", ins.JobsCount.data[0]);
 
-    calculateThreatLevel(JSON.parse(ins.Ranks.data[0].replace("=>", ":")));
+    ranks = ins.Ranks.data[0].stdout.replace(/=>/g, ":").trim().split("\n");
+    var threatLevel = calculateThreatLevel(ranks);
+
+    //TODO: update threat level for this
 
     var countLeft = ins.JobsCount.data[0];
 
@@ -27,7 +51,7 @@ function computeThreatLevel(ins, outs, config, cb) {
         process.exit();
     }
 
-    outs.JobsCount.data = [ countLeft-1 ];
+    outs.JobsCount.data = [ countLeft - 1 ];
 
     cb(null, outs);
 }
