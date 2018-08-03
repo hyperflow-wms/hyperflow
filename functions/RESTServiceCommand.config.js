772b1a6b8e4517b5e265335914163502b7cfd394
Maciej Pawlik <m.pawlik@cyfronet.pl>
2018-08-03 21:41:12 +0200

    Rename env vars for passing configuration to RESTService.

--- functions/RESTServiceCommand.config.js
+++ functions/RESTServiceCommand.config.js
@@ -1,8 +1,8 @@
 var SERVICE_URL = process.env.SERVICE_URL ? process.env.SERVICE_URL : "https://localhost:2000/";
 
 var STORAGE = process.env.STORAGE ? process.env.STORAGE : "aws";
-var BUCKET = process.env.BUCKET ? process.env.BUCKET : "bucket";
-var PATH = process.env.PATH ? process.env.PATH : "data"; //prefix in a bucket with no leading or trailing slashes
+var BUCKET = process.env.S3_BUCKET ? process.env.S3_BUCKET : "bucket";
+var PATH = process.env.S3_PATH ? process.env.S3_PATH : "data"; //prefix in a bucket with no leading or trailing slashes
 
 exports.service_url = SERVICE_URL;
 
