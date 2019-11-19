12be5557bc1f33b85dc6ac17d855413def28279a
Bartosz Balis <balis@agh.edu.pl>
2019-11-19 23:50:30 +0100

    Fix formatting

--- examples/TaskStatusRedis/functions.js
+++ examples/TaskStatusRedis/functions.js
@@ -3,35 +3,35 @@ var spawn = require('child_process').spawn;
 // Spawns a job "node handler.js" and waits for the notification of its
 // completion using the Redis task status notification mechanism
 async function task_status_redis_test(ins, outs, context, cb) {
-    var n = Number(ins.number.data[0]); 
+  var n = Number(ins.number.data[0]);
 
-    //console.log("Spawning process...");
+  //console.log("Spawning process...");
 
-    var proc = spawn('node', ['handler.js', context.taskId, context.redis_url]);
+  var proc = spawn('node', ['handler.js', context.taskId, context.redis_url]);
 
-    proc.stderr.on('data', function(data) {
-	console.log(data.toString());
-    });
+  proc.stderr.on('data', function(data) {
+    console.log(data.toString());
+  });
 
-    proc.stdout.on('data', function(data) {
-	console.log(data.toString());
-    });
+  proc.stdout.on('data', function(data) {
+    console.log(data.toString());
+  });
 
-    proc.on('exit', function(code) {
-	//console.log('Process exited with code', code);
-    });
+  proc.on('exit', function(code) {
+    //console.log('Process exited with code', code);
+  });
 
-    // wait for the task to finish (timeout=0 means indefinite)
-    try {
-        var taskStatus = await context.taskStatus(0);
-	console.log('Received task status:', taskStatus);
-        setTimeout(function() {
-	    cb(null, outs);
-	 }, 5000);
-    } catch(err) {
-        console.err(err);
-	throw err;
-    }
+  // wait for the task to finish (timeout=0 means indefinite)
+  try {
+    var taskStatus = await context.taskStatus(0);
+    console.log('Received task status:', taskStatus);
+    setTimeout(function() {
+      cb(null, outs);
+    }, 5000);
+  } catch(err) {
+    console.err(err);
+    throw err;
+  }
 }
 
 exports.task_status_redis_test = task_status_redis_test;
