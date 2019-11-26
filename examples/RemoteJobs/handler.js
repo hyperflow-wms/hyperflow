b9a52e6b33fa03ffc95b822d9175220143598222
Bartosz Balis <balis@agh.edu.pl>
2019-11-26 21:59:50 +0100

    Formatting

--- examples/RemoteJobs/handler.js
+++ examples/RemoteJobs/handler.js
@@ -16,56 +16,56 @@ redis_url = process.argv[3];
 var rcl = redis.createClient(redis_url);
 
 // get job message from Redis 
-var getJobMessage = async function(timeout) {
-    return new Promise(function(resolve, reject) {
-            const jobMsgKey = taskId + "_msg";
-            rcl.brpop(jobMsgKey, timeout, function(err, reply) {
-                    if (err) reject(err)
-                    else {
-                        resolve(reply);
-                    }
-                });
-            });
+var getJobMessage = async function (timeout) {
+    return new Promise(function (resolve, reject) {
+        const jobMsgKey = taskId + "_msg";
+        rcl.brpop(jobMsgKey, timeout, function (err, reply) {
+            if (err) reject(err)
+            else {
+                resolve(reply);
+            }
+        });
+    });
 }
 
 // send notification about job completion to Redis
-var notifyJobCompletion = async function() {
-    return new Promise(function(resolve, reject) {
-            rcl.rpush(taskId, "OK", function(err, reply) {
-                if (err) reject(err)
-                else {
-                    resolve(reply);
-                }
-            });
+var notifyJobCompletion = async function () {
+    return new Promise(function (resolve, reject) {
+        rcl.rpush(taskId, "OK", function (err, reply) {
+            if (err) reject(err)
+            else {
+                resolve(reply);
+            }
+        });
     });
 }
 
 
 async function executeJob() {
-    
+
     // 1. get job message
     try {
         var jobMessage = await getJobMessage(10);
-    } catch(err) {
-      console.error(err);
-      throw err;
+    } catch (err) {
+        console.error(err);
+        throw err;
     }
     console.log("Received job message:", jobMessage);
 
     // 2. HERE the job would be executed
 
     // 3. Notify job completion (delay simulates execution time)
-    var delay=Math.random()*3000;
+    var delay = Math.random() * 3000;
     // console.log("Delay:", delay);
-    setTimeout(async function() {
-            try {
-                await notifyJobCompletion();
-            } catch(err) {
-                console.error("Redis notification failed", err);
-                throw err;
-            }
+    setTimeout(async function () {
+        try {
+            await notifyJobCompletion();
+        } catch (err) {
+            console.error("Redis notification failed", err);
+            throw err;
+        }
 
-            process.exit(0);
+        process.exit(0);
     }, delay);
 }
 
