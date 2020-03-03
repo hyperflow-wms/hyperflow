e2f060e3eaa13a8fe365187c5d4af4c8379b22f6
Bartosz Balis <balis@agh.edu.pl>
2020-03-03 12:53:51 +0000

    Add 'job.js' that induces CPU load

---
+++ examples/RemoteJobs/job.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+// 3 seconds of high CPU load and 3 seconds of no CPU load
+
+var shouldRun = true;
+
+var t0 = (new Date()).getTime();
+
+function loadCpu(ms) {
+	var result = 0;
+	for (i=0; i<100; i++) {
+	   result += Math.random() * Math.random();
+	}
+	var now = new Date().getTime();
+	if (now - t0 > ms) {
+		setTimeout(function() {
+			process.exit(0);
+		}, 3000);
+		return;
+	}
+	process.nextTick(loadCpu, ms);
+}
+
+loadCpu(3000);
+
+
