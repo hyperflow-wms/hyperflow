39c63d20595c16611294bab2f96f229591dfe771
Bartosz Balis <balis@agh.edu.pl>
2021-04-23 11:19:59 +0200

    Add usage of async scheduler API

--- examples/SchedulerDemo/functions.js
+++ examples/SchedulerDemo/functions.js
@@ -1,4 +1,7 @@
-async function taskFunction(ins, outs, context, cb) {
+const { newContexts } = require("@kubernetes/client-node/dist/config_types");
+
+// Using synchronous scheduler API
+async function taskFunction1(ins, outs, context, cb) {
 	let scheduler = context.appConfig.scheduler;
 	if (scheduler) {
 		var node = await scheduler.getTaskExecutionPermission(context.appId, context.procId);
@@ -13,4 +16,32 @@ async function taskFunction(ins, outs, context, cb) {
 	cb(null, outs);
 }
 
-exports.taskFunction = taskFunction;
+
+// Using asynchronous scheduler API
+function taskFunction2(ins, outs, context, cb) {
+	let taskItem = { 
+		"ins": ins, 
+		"outs": outs, 
+		"context": context, 
+		"cb": cb
+	}
+
+	let scheduler = context.appConfig.scheduler;
+
+	if (scheduler) {
+		return scheduler.addTaskItem(taskItem, taskFunction2Cb);
+	}
+
+}
+
+function taskFunction2Cb(taskArray, node) {
+	console.log("Got scheduler callback, executing task array on node", node);
+	taskArray.forEach((task) => {
+		let scheduler = task.context.appConfig.scheduler;
+		scheduler.notifyTaskCompletion(task.context.appId, task.context.procId);
+		task.cb(null, task.outs);
+	});
+}
+
+exports.taskFunction1 = taskFunction1;
+exports.taskFunction2 = taskFunction2;
